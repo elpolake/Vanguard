8e9e1c95dee7e71fd40f2b6a6a95bbf00f3dd96a
Spilios Maderakis <smaderakis06@gmail.com>
2024-07-17 23:33:09 +0300

    Updated To Minecraft Version 1.21

--- src/main/java/me/spilios/vanguard/module/Category.java
+++
@@ -1,5 +0,0 @@
-package me.spilios.vanguard.module;
-
-public enum Category {
-    COMBAT, MOVEMENT, RENDER, PLAYER, MISC, CLIENT
-}
--- src/main/java/me/spilios/vanguard/settings/SettingBool.java
+++
@@ -1,8 +0,0 @@
-package me.spilios.vanguard.settings;
-
-public class SettingBool {
-    public boolean status;
-    public SettingBool(boolean status){
-        this.status = status;
-    }
-}
